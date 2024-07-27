3c1a82dca817816ef731c2a339a89d7daa08ce0a
testanderror <108211073+testanderror@users.noreply.github.com>
2024-07-27 14:37:24 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -763,10 +763,10 @@ function App() {
               >
                 <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
                   <p className="text-[#00df9a] font-bold p-2">
-                    VIAJA Y EXPLOREMOS JUNTOS
+                    TOURES EN ESPAÑOL
                   </p>
                   <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
-                    TOURES GUIADOS EN{' '}
+                    Exploremos juntos{' '}
                     <span className="w-full text-3xl font-bold text-[#00df9a]">
                       KYOTO, NARA, KOBE
                     </span>{' '}
